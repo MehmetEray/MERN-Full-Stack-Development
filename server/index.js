3fa4860bfe1beb9a0dc1f6468014825ade60bdbb
eraysurmeli <surmelimehmeteray@gmail.com>
2021-03-08 17:12:09 +0300

    structure set

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from './routes/posts.js';
+const app = express();
+
+app.use('/posts',postRoutes);
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
+
+//https://www.mongodb/cloud/atlas
+const CONNECTION_URL =
+    "mongodb+srv://mehmeteray:mehmeteray123@cluster0.cpokm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+mongoose
+    .connect(CONNECTION_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() =>
+        app.listen(PORT, () =>
+            console.log(`MongoDB Cluster connected! Server Running on Port: http://localhost:${PORT}`)
+        )
+    )
+    .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set("useFindAndModify", false);
