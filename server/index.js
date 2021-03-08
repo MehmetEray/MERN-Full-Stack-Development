8add13ec25d1da98819096a62839e7bdc25a21b2
eraysurmeli <surmelimehmeteray@gmail.com>
2021-03-08 17:56:29 +0300

    mongo connect change

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 
 //https://www.mongodb/cloud/atlas
 const CONNECTION_URL =
-    "mongodb+srv://mehmeteray:mehmeteray123@cluster0.cpokm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    "mongodb+srv://mehmeteray:mehmeteray12@cluster0.cpokm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 mongoose
     .connect(CONNECTION_URL, {
